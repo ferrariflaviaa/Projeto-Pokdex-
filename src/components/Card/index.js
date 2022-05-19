bb35076a2ebfea4bda2d3d4ba7e884fa5424c9b6
Flávia Ferrari <https://github.com/ferrariflaviaa>
2022-05-19 17:36:06 -0300

    estrutura do header pronta

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+
+export default function Card(){
+    
+
+    return(
+        <div className='containerCard'>
+            <h1>tygedyubuybduy</h1>
+        </div>
+    )
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+export default function Header() {
+
+
+    return (
+        <div className="container">
+
+            <div className='button' >
+                <button className='styleB'>VER MINHA POKEDEX</button>
+            </div>
+
+            <div className='list'>
+                <h2>LISTA DE POKEMOS</h2>
+            </div>
+
+        </div>
+    )
+
+}
