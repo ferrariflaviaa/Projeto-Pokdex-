7e308196aa3d48e1650e50e4bdaa3a50bc0cd086
Flávia Ferrari <https://github.com/ferrariflaviaa>
2022-05-20 15:38:28 -0300

    estilização card 

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,25 @@
 import './style.css';
+import pikachu from './../../img/Pikachu.jpg'
 
 
-export default function Card(){
-    
+export default function Card() {
 
-    return(
+
+    return (
         <div className='containerCard'>
-            <h1>tygedyubuybduy</h1>
+            <div className='column'>
+                <div className='styledCard'>
+                    <img className='imgCard' src={pikachu} alt='pokemon' />
+
+                    <div className='bottomCard'>
+                        <bottom className='adicionarCard'>Adicionar</bottom><br/>
+                    </div>
+
+                    <div className='bottomCard'>
+                        <bottom className='detalherCard'>Ver Detalher</bottom>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
